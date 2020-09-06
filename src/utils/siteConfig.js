bc291664e6222486f16195eba5cc4ae6ce6d699c
John Zambrano <johnnyz88@gmail.com>
2020-09-06 17:40:46 +1000

    siteurl changed to https

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://blog.johnzambrano.dev`, // Site domain. Do not include a trailing slash!
+	siteUrl: `https://blog.johnzambrano.dev`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
